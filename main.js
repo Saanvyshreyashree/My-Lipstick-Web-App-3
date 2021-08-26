effbb1cf7fd9e4698f9997cba0196fda498000ec
Saanvyshreyashree <77281383+Saanvyshreyashree@users.noreply.github.com>
2021-08-26 06:37:18 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,53 @@
+noseX=0;
+noseY=0; 
+
+function preload()
+{
+   lipstick_nose = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+
 function setup()
 {
-    canvas=createCanvas(640,480);
-    canvas.position(110,250);
-    video=createCapture(VIDEO);
-    video.hide();
-    tintc="";
+   canvas= createCanvas(300,300);
+   canvas.center();
+
+   video= createCapture(VIDEO);
+   video.size(300,300);
+   video.hide();
+
+   poseNet=ml5.poseNet(video,modelLoaded);
+
+   poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+   console.log('poseNet is initialized'); 
+}
+
+function gotPoses(results)
+{
+   if(results.length > 0)
+   {
+      console.log(results);
+      console.log("nose x="+results[0].pose.nose.x);
+      console.log("nose y="+results[0].pose.nose.y);
+      noseX=results[0].pose.nose.x;
+      noseY=results[0].pose.nose.y;
+   }
 }
 
 function draw()
 {
-   image(video,0,0,640,480);
-   tint(tintc);
+   image(video,0,0,300,300);
+   image(lipstick_nose, noseX-10, noseY+15, 30, 30 );
 }
 
-function filter_tint()
-{
-   tintc=document.getElementById("color_name").value;
-}
+
+
+
 
 function take_snapshot()
 {
-    save('student.png');
+   save('myfilterimage.png');
 }
